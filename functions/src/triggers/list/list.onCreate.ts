2f5d6d738f53f7db8d752f536d6c49b4d9e70558
yakut54 <mongodb1@yandex.ru>
2025-10-26 08:28:36 +0700

    onCreateList maxOrder

--- functions/src/triggers/list/list.onCreate.ts
+++ functions/src/triggers/list/list.onCreate.ts
@@ -1,7 +1,7 @@
 import { logger } from 'firebase-functions'
 import { onDocumentCreated } from 'firebase-functions/v2/firestore'
 import { List } from 'src/types/models/todo-model'
-import type { DocumentReference } from 'firebase-admin/firestore'
+import { DocumentReference, Timestamp } from 'firebase-admin/firestore'
 import { createConverter } from 'src/triggers/list/utils/createConverter'
 import { firestore } from 'src/lib/firebaseAdmin'
 
@@ -14,19 +14,49 @@ export const onCreateList = onDocumentCreated(
   async (event) => {
     const listId: string = event.params.listId
     const listRef = event.data?.ref as DocumentReference<List> | undefined
+    const incomingData: Partial<List> = event.data?.data() || {}
+    const ownerId = incomingData.owner_id
 
     if (!listRef) {
       logger.warn('No list reference', { listId })
       return
     }
 
-    const snapshot = await firestore
-      .collection('lists')
-      .withConverter(createConverter<List>())
-      .get()
+    if (!ownerId) {
+      logger.warn('Owner ID is missing')
+      return
+    }
 
-    const allLists: List[] = snapshot.docs.map((doc) => doc.data())
-    logger.debug('All lists', { allLists })
+    try {
+      const maxOrderSnapshot = await firestore
+        .collection('lists')
+        .withConverter(createConverter<List>())
+        .where('owner_id', '==', ownerId)
+        .orderBy('order', 'desc')
+        .limit(1)
+        .get()
+
+      const maxOrder = maxOrderSnapshot.empty ? 0 : (maxOrderSnapshot.docs[0].data().order || 0)
+      const order = maxOrder + 1
+
+      logger.debug('order', { order })
+
+      const defaultValue: List = {
+        owner_id: ownerId,
+        title: incomingData.title ?? 'WTF???',
+        color: incomingData.color ?? '#f044dd',
+        icon: incomingData.icon ?? 'person',
+        created_time: Timestamp.now(),
+        updated_time: Timestamp.now(),
+        is_archived: false,
+        task_count: 0,
+        order,
+      }
+
+      logger.debug('defaultValue', { defaultValue })
+    } catch (e) {
+      logger.error(e)
+    }
   },
 )
 
